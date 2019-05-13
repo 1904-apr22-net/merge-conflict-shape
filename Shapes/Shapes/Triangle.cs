d6c136a5ce7b9ae4fab884c940f991485af0354a
Justin Wright <justinmwright96@gmail.com>
2019-05-13 14:04:17 -0500

    added get/set

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -6,9 +6,9 @@ namespace Shapes
 {
     class Triangle
     {
-        public int side1;
-        public int side2;
-        public int side3;
+        public int side1 { get; set; }
+        public int side2 { get; set; }
+        public int side3 { get; set; }
         public Triangle(int side1, int side2, int side3)
         {
             side1 = this.side1;
