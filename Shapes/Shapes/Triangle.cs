cead4656eedae5bd5ac930178e45daeea839d51b
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-05-13 13:39:09 -0500

    implement triangle class

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Shapes
 {
-    class Triangle
+    public class Triangle
     {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public double Perimeter => A + B + C;
+
+        public double Area => Math.Sqrt(Perimeter / 2 *
+            (Perimeter / 2 - A) * (Perimeter / 2 - B) * (Perimeter / 2 - C));
+
+        public TriangleType Type
+        {
+            get
+            {
+                var numEqualSides = new[] { A, B, C }.Distinct().Count();
+                switch (numEqualSides)
+                {
+                    case 1: return TriangleType.Equilateral;
+                    case 2: return TriangleType.Isosceles;
+                    default: return TriangleType.Scalene;
+                }
+            }
+        }
     }
 }
---
+++ Shapes/Shapes/TriangleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public enum TriangleType
+    {
+        Scalene,
+        Isosceles,
+        Equilateral
+    }
+}
