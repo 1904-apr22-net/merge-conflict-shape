efc2743775668b60f070946a599c4efce0019b2b
Bill phung <b.giaphung@gmail.com>
2019-05-13 13:46:09 -0500

    triangles

---
+++ Shapes/Triangles.Library/Perimeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangles.Library
+{
+    public class Perimeter
+    {
+        public int GetPerimeter(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
