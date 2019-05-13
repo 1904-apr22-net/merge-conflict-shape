ff83185c55bfddbcdffe296a8fea183135dc8bc2
Logan Westbrook <lmw170330@utdallas.edu>
2019-05-13 12:29:07 -0500

    add stuff

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -6,5 +6,17 @@ namespace Shapes
 {
     class Triangle
     {
+	public double Base{ get; set; }
+	public double Height{ get; set; }
+
+	public Triangle(double base, double Height)
+	{
+		Base = base;
+		height = height;
+	}
+	public double GetArea()
+	{
+		return(Base * .5 * Height;
+	}
     }
 }
