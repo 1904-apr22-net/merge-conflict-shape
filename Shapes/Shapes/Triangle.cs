b82741be8a4afc38226160268d587477d97ea4f0
unknown <rodsalomon@utexas.edu>
2019-05-13 12:30:59 -0500

    Added methods to triangle class

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -6,5 +6,39 @@ namespace Shapes
 {
     class Triangle
     {
+        int Height { get; set; }
+        int BaseSide { get; set; }
+
+        int Side1 { get; set;}
+        int Side2 { get; set; }
+
+        public int area()
+        {
+            return ((this.Height * this.BaseSide) / 2);
+        }
+
+        public bool isEquilateral()
+        {
+            if(this.Side1== this.Side2 && this.Side1 == this.BaseSide)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isScalene()
+        {
+            if (this.Side1 == this.Side2 && this.Side1 != this.BaseSide)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
